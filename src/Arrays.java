115fd3708ed5be663376b1d7429e15df5997d584
obidkon <obid.sharipov.80@mail.ru>
2020-11-23 22:17:05 +0500

    Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our zombic killer...");
+String backbag[] ={"Miltiq ","autamat","Sniyper"};
+String zombies[]={"Smole-zommbiy","lorge-zombie", "Strong-zombeie"};
+      int nuberZombyeHate[]={4 ,90, 78, 123 , 12 ,561,1313 ,1};
+        System.out.println(nuberZombyeHate[5]);
+
+
+    }
+}
